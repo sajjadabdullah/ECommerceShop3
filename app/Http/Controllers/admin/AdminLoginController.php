4bbda2fea87f2e5f1aee27e6f1364c5f3503fff7
Sajjad Abdullah <38566464+sajjadabdullah@users.noreply.github.com>
2023-11-14 18:03:06 +0500

    aa

--- app/Http/Controllers/admin/AdminLoginController.php
+++ app/Http/Controllers/admin/AdminLoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
@@ -23,7 +24,12 @@ class AdminLoginController extends Controller
 
         if ($validator->passes())
         {
+            if (Auth::guard('admin')->attempt(
+                ['email'=>$request->email, 'password'=>$request->password],
+                $request->get('remember')))
+                {
 
+                }
         }
         else
         {
